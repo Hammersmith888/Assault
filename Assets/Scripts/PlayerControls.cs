2693ac974c8f5d0dd4b8638abe1cd05bb060c398
Hammersmith888 <vladislavv8@mail.ru>
2022-03-13 00:46:19 +0300

    Commint int
    into GIT

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 3.5f;
+    
+    void Update()
+    {
+        
+
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+        
+
+        
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+
+        float rawYPos = transform.localPosition.x + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3 (clampedXPos, clampedYPos, transform.localPosition.z);
+        
+    }
+}
